12861b33397482030aa4446674c99187fa818ce4
minnymaster <113033210+minnymaster@users.noreply.github.com>
2025-03-24 12:02:04 +0500

    Update main.py

--- Predict/main.py
+++ Predict/main.py
@@ -9,42 +9,32 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 df = pd.read_csv('education_career_success.csv')
 
-
 df = pd.get_dummies(df, drop_first=True)
-
 
 X = df.drop(columns=['Entrepreneurship_Yes'])  # Замените на нужный целевой признак
 y = df['Entrepreneurship_Yes']  # Целевой признак
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 smote = SMOTE(random_state=42)
 X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
-
 
 scaler = StandardScaler()
 X_train_res_scaled = scaler.fit_transform(X_train_res)
 X_test_scaled = scaler.transform(X_test)
 
-
 xgb_model = xgb.XGBClassifier(scale_pos_weight=10, random_state=42, use_label_encoder=False)
-
 
 xgb_model.fit(X_train_res_scaled, y_train_res)
 
-# Прогнозируем на тестовой выборке
 y_pred = xgb_model.predict(X_test_scaled)
 
-# Оценка модели
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 roc_auc = roc_auc_score(y_test, xgb_model.predict_proba(X_test_scaled)[:, 1])
 
-# Вывод результатов
 print(f"Accuracy: {accuracy}")
 print(f"Precision: {precision}")
 print(f"Recall: {recall}")
